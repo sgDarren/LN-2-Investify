7c3975cdd0a57e4980ac56dd2e69307bb1ae9650
sgDarren <glatzdar@students.zhaw.ch>
2025-06-02 17:56:22 +0200

    Configure for Netlify deployment

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/svelte";
-import { AUTH_BASE_URL } from '$env/static/public';
+import { PUBLIC_AUTH_BASE_URL } from '$env/static/public';
 
 export const authClient = createAuthClient({
-  baseURL: AUTH_BASE_URL 
+  baseURL: PUBLIC_AUTH_BASE_URL
 });
